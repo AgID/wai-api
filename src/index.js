5e4e88d8346e94eea1e69b33f1f989b14d21a0b4
Davide Porrovecchio <pdavide@gmail.com>
2021-02-17 21:27:38 +0100

    Split app and index files

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
-import express from "express";
-import bodyParser from "body-parser";
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import dotenv from "dotenv";
-import routes from "./routes/routes";
-
-export const app = express();
+import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
-
-app.use(bodyParser.json());
-
-app.use(routes);
-
-app.use((req, res)=>{
-  res.status(404).json({ error: "Not Found" });
-});
 
 let port;
 
 switch (process.env.NODE_ENV) {
-  case "dev":
-    port = process.env.PORT_DEV
+  case 'dev':
+    port = process.env.PORT_DEV;
     break;
-  case "test":
-    port = process.env.PORT_TEST
+  case 'test':
+    port = process.env.PORT_TEST;
     break;
   default:
-    port = process.env.PORT_PROD
+    port = process.env.PORT_PROD;
     break;
 }
 
-export const server = app.listen(port, () => {
+export default app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`app is listening to port ${port}`);
 });
