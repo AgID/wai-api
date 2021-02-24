2158991e157295448f76313a0b0803ce90a6a346
Davide Porrovecchio <pdavide@gmail.com>
2021-02-24 19:37:30 +0100

    Refactor parameters validation code
    - allow module and methos params only once
    - collect all messages in a proper object
    - use significant http response status codes

--- src/controllers/middleware/isMethodAllowed.js
+++ src/controllers/middleware/isMethodAllowed.js
@@ -1,61 +1,53 @@
 import config from '../../config/config';
+import messages from '../../utils/messages';
 
 export default (queryModule, queryMethod, permission) => {
   const error = {
     error: true,
-    message: '',
+    status: 403,
+    message: messages.errors.forbidden,
   };
 
-  if (!queryModule[0] || !queryMethod[0]) {
-    error.message = 'no "module" or "method" specified';
+  if (!queryModule || !queryMethod) {
+    error.message = messages.errors.malformedParameters;
     return error;
   }
 
   const matomoConfig = config.matomo;
 
   if (!matomoConfig?.enabled || Object.keys(matomoConfig.enabled).length < 1) {
-    error.message = "no allowed 'module' was found in the configuration file";
-    return error;
-  }
-  // eslint-disable-next-line
-  for (const module of queryModule) {
-    const moduleKey = Object.keys(matomoConfig.enabled).find(
-      (elem) => elem.toLowerCase() === module.toLowerCase(),
-    );
-    const moduleList = matomoConfig.enabled[moduleKey];
+    error.message = messages.errors.misconfiguredServer;
+    error.status = 500;
 
-    if (moduleList) {
-      // eslint-disable-next-line
-      for (const method of queryMethod) {
-        const methodKey = Object.keys(moduleList).find(
-          (elem) => elem.toLowerCase() === method.toLowerCase(),
-        );
-
-        if (!moduleList[methodKey]) {
-          error.error = true;
-          error.message = `Can't access method '${method}'`;
-          break;
-        }
-
-        if (moduleList[methodKey] === permission || permission === 'admin' || permission === 'RW') {
-          error.error = false;
-        } else {
-          error.error = true;
-          error.message = `Not allowed to access method '${method}' with the current permission settings`;
-          break;
-        }
-      }
-      if (error.error) break;
-    } else {
-      error.error = true;
-      error.message = `Module '${module}' not allowed`;
-      break;
-    }
-  }
-
-  if (error.error) {
     return error;
   }
 
-  return { error: false };
+  const moduleKey = Object.keys(matomoConfig.enabled).find(
+    elem => elem.toLowerCase() === queryModule.toLowerCase(),
+  );
+  const modulesList = matomoConfig.enabled[moduleKey];
+
+  if (!modulesList) {
+    error.error = true;
+
+    return error;
+  }
+
+  const methodKey = Object.keys(modulesList).find(
+    elem => elem.toLowerCase() === queryMethod.toLowerCase(),
+  );
+
+  if (!modulesList[methodKey]) {
+    error.error = true;
+
+    return error;
+  }
+
+  if (modulesList[methodKey] === permission || permission === 'admin' || permission === 'RW') {
+    error.error = false;
+  } else {
+    error.error = true;
+  }
+
+  return error;
 };
--- src/controllers/middleware/isUserAllowed.js
+++ src/controllers/middleware/isUserAllowed.js
@@ -1,5 +1,12 @@
+import messages from '../../utils/messages';
+
+const notAllowed = () => ({
+  error: true,
+  message: messages.errors.forbidden,
+});
+
 export default (headers, idSite) => {
-  if (Number.isNaN(idSite)) return { error: true, message: 'idSite not valid' };
+  if (Number.isNaN(idSite)) return { error: true, message: messages.errors.invalidSiteId };
 
   const consumerCustomId = headers['x-consumer-custom-id'];
 
@@ -9,40 +16,40 @@ export default (headers, idSite) => {
 
     try {
       customId = JSON.parse(headers['x-consumer-custom-id']);
-      const isAdmin = customId.type === 'admin';
-      const hasIdList = Array.isArray(customId.siteId);
-
-      if (isAdmin) {
-        permission = 'admin';
-      } else if (!isAdmin && hasIdList) {
-        if (idSite > 0) {
-          permission = customId.siteId.find((elem) => elem.id === idSite);
-          permission = permission?.permission;
-          if (!permission) {
-            return {
-              error: true,
-              message: `Not allowed to access website with idSite '${idSite}'`,
-            };
-          }
-        } else {
-          permission = 'R';
-        }
-      } else {
-        permission = false;
-      }
     } catch (error) {
       return {
         error: true,
-        message: 'x-consumer-custom-id must be in JSON format',
+        status: 500,
+        message: messages.errors.internalServerError,
       };
     }
 
-    if (permission) {
-      return { error: false, permission };
+    const isAdmin = customId.type === 'admin';
+    const hasIdList = Array.isArray(customId.siteId);
+
+    if (isAdmin) {
+      permission = 'admin';
+    } else if (!isAdmin && hasIdList) {
+      if (idSite !== -1) {
+        const site = customId.siteId.find(elem => elem.id === idSite);
+        permission = site?.permission;
+
+        if (!permission) {
+          return notAllowed();
+        }
+      } else {
+        permission = 'R';
+      }
+    } else {
+      return notAllowed();
     }
+
+    return { error: false, permission };
   }
+
   return {
     error: true,
-    message: 'Failed to find credential',
+    status: 500,
+    message: messages.errors.internalServerError,
   };
 };
