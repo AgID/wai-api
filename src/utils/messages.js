b7e16206d2a87914362550e286c5d4585f6f051c
Davide Porrovecchio <pdavide@gmail.com>
2021-04-13 09:56:02 +0200

    Use single quotes in messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,8 +1,8 @@
 export default {
   errors: {
     forbidden: 'Request is forbidden to the provided credential',
-    malformedParameters: '"Module" and "method" parameters are required and should be specified only once.',
-    invalidSiteId: 'Provided "idSite" is not valid',
+    malformedParameters: "'Module' and 'method' parameters are required and should be specified only once.",
+    invalidSiteId: "Provided 'idSite' is not valid",
     misconfiguredServer: 'Error in server configuration.',
     internalServerError: 'Internal server error',
   },
