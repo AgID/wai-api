458acc3f28ff97fe3806a41cc738ff9dd16f1f65
Davide Porrovecchio <pdavide@gmail.com>
2022-02-25 21:43:48 +0100

    Use matomo admin token in backend

--- src/controllers/matomo.js
+++ src/controllers/matomo.js
@@ -55,7 +55,7 @@ export default async (req, res) => {
   }
 
   const { method } = req;
-  const url = process.env.ANALYTICS_PUBLIC_URL + encodeURI(params);
+  const url = `${process.env.MATOMO_SERVICE_URL}${encodeURI(params)}&token_auth=${process.env.MATOMO_ADMIN_TOKEN_AUTH}`;
 
   const response = await apiCall({ method, url, data: {} });
 
